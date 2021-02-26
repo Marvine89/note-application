9ba9993d654830964455545a3f2be00d90a0f58a
Marvine Bamba <mbamba@jhb.dvt.co.za>
2021-02-26 17:55:25 +0200

    add: router login to app

--- src/frontend/src/routes/routes.tsx
+++ src/frontend/src/routes/routes.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Dashboard } from "../screens/dashboard";
+import { NotFound } from "../screens/not-found";
+import { ViewNotes } from "../screens/view-notes";
 
-export const RootRouter: React.FC = () => {
-  return null;
+export const AppRouter: React.FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/users" component={ViewNotes} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
+  );
 };
