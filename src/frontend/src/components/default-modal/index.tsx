0cb4e9329ab9fcdf1564e7bd2c71b3891f0d3ada
Marvine Bamba <mbamba@jhb.dvt.co.za>
2021-02-28 19:50:12 +0200

    refactor: default modal

--- src/frontend/src/components/default-modal/index.tsx
+++ src/frontend/src/components/default-modal/index.tsx
@@ -1,39 +1,7 @@
 import React from "react";
 import { Backdrop } from "@material-ui/core";
-
-import { useSpring, animated } from "react-spring/web.cjs";
+import Fade from "@material-ui/core/Fade";
 import { CloseIconModal, ModalCloseIconBlock, ModalContainer, ModalWrapper } from "./styles";
-
-interface FadeProps {
-  children?: React.ReactElement;
-  in: boolean;
-  onEnter?: () => {};
-  onExited?: () => {};
-}
-
-const Fade = React.forwardRef<HTMLDivElement, FadeProps>(function Fade(props, ref) {
-  const { in: open, children, onEnter, onExited, ...other } = props;
-  const style = useSpring({
-    from: { opacity: 0 },
-    to: { opacity: open ? 1 : 0 },
-    onStart: () => {
-      if (open && onEnter) {
-        onEnter();
-      }
-    },
-    onRest: () => {
-      if (!open && onExited) {
-        onExited();
-      }
-    },
-  });
-
-  return (
-    <animated.div style={style} {...other}>
-      {children}
-    </animated.div>
-  );
-});
 
 interface DefaultModalProps {
   open: boolean;
