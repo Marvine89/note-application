9f8a4f9e74ff8695e27d86a96ccf92f277c565de
Marvine Bamba <mbamba@jhb.dvt.co.za>
2021-02-28 20:23:32 +0200

    fix: build script

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
 const Router = require('express').Router();
-import { API_Router } from "./api/api-routes"
-import { AuthMiddleware } from "./api/middlewares"
+import { API_Router } from "./api/api-routes";
+import { AuthMiddleware } from "./api/middlewares";
+var fs = require('fs');
 
 Router.get('/', function (_, res) {
-  res.send('Express + TypeScript Server')
+  // check for frontend build file from npm run build
+  if (fs.existsSync(__dirname + "/frontend/index.html")) {
+    res.sendFile('frontend/index.html', { root: __dirname })
+  } else {
+    res.status(404).send("Front-end build folder not found");
+  }
 });
 
 Router.use('/api', [AuthMiddleware], API_Router);
